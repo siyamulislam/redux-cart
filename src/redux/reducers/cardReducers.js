51038445a47d5504b799d706f347f6824401634c
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-03 04:08:33 +0600

    format documents

--- src/redux/reducers/cardReducers.js
+++ src/redux/reducers/cardReducers.js
@@ -1,13 +1,12 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cartActions";
-
 const initialState = {
     cart: [],
     products: [
-        { name: 'Lenovo', id: 1},
-        { name: 'HP', id: 2},
-        { name: 'Asus', id: 3},
-        { name: 'Dell', id: 4},
-        { name: 'Mi', id: 5},
+        { name: 'Lenovo', id: 1 },
+        { name: 'HP', id: 2 },
+        { name: 'Asus', id: 3 },
+        { name: 'Dell', id: 4 },
+        { name: 'Mi', id: 5 },
     ]
 }
 const cartReducer = (state = initialState, action) => {
@@ -16,16 +15,15 @@ const cartReducer = (state = initialState, action) => {
             const newItem = {
                 productID: action.id,
                 name: action.name,
-                cartID:state.cart.length
-            } 
-            const newCart = [...state.cart,newItem]; 
-            return { cart: newCart , products:state.products};
-        case REMOVE_FROM_CART:  
+                cartID: state.cart.length
+            }
+            const newCart = [...state.cart, newItem];
+            return { cart: newCart, products: state.products };
+        case REMOVE_FROM_CART:
             const remainCart = state.cart.filter(item => item.cartID !== action.cartID);
-            return { ...state,cart: remainCart };
+            return { ...state, cart: remainCart };
         default:
             return state;
     }
 }
-
 export default cartReducer;
